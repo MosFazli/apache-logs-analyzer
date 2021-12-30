3fb29e2202c531c9e5fb69526a7796d7b85742a1
Mostafa Fazli <MosFazli@gmail.com>
2021-12-30 07:06:23 +0330

    added menu control and get file

--- analyzer.sh
+++ analyzer.sh
@@ -2,5 +2,47 @@
 
 clear
 
+function showIPs {
+    cat $log | awk '{ print $1}'
+}
+
+
+function analyzeMenu {
+
+echo Analyzing logs from "$log" file
+
+while true
+do
+    echo 
+    echo -e "\n***************************\n"
+    echo -e "choose an option: \n"
+    echo    "1- Show All IPs"
+    read selection
+    
+    case $selection in
+    1) showIPs;;
+    *) echo you Entered Invalid Selection, try again;;
+    
+    esac
+    
+done
+}
+
+function getFile {
+echo -e "Please select the input log:\n"
+ls -p | grep -w log
+echo
+read log
+
+if [ ! -f $log ]; then
+    echo -e "\nFile not found!\n"
+    echo -e "Please try again and make sure log file is available in directory"
+    getFile
+else
+    analyzeMenu
+fi
+}
+
 echo "*************************"
-echo "This Program can be analyze "
+echo -e "This Program can be analyze a apache log file\n"
+getFile
